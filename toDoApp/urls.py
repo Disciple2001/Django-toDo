c23a2403a481a93fc180b70482668f70042cf6a5
Disciple2001 <dariohabana2001@gmail.com>
2024-09-26 12:00:20 -0400

    todo crud completed

--- toDoApp/urls.py
+++ toDoApp/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     # Todo urls
-    path('todo/addTask', views.addTask, name='addTask')
+    path('todo/addTask', views.addTask, name='addTask'),
+    path('todo/mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
+    path('todo/mark_as_undone/<int:pk>', views.mark_as_undone, name='mark_as_undone'),
+    path('todo/editTask/<int:pk>', views.editTask, name='editTask'),
+    path('todo/deleteTask/<int:pk>', views.deleteTask, name='deleteTask')
 
 ]
--- toDoApp/views.py
+++ toDoApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from toDoApp import models
 
@@ -8,8 +8,11 @@ from toDoApp import models
 
 def home(request):
     tasks = models.Task.objects.filter(status=1).order_by('-created_at')
+    completed_tasks = models.Task.objects.filter(status=2).order_by('-created_at')
+
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks
     }
     return render(request, 'home.html', context)
 
@@ -17,4 +20,42 @@ def home(request):
 def addTask(request):
     task = request.POST['task']
     models.Task.objects.create(task=task)
-    return HttpResponse('Form submitted')
+    return redirect('home')
+
+
+def mark_as_done(request, pk):
+    # task = models.Task.objects.filter(pk=pk)
+    # task.update(status=2)
+
+    task = get_object_or_404(models.Task, pk=pk)
+    task.status = 2
+    task.save()
+
+    return redirect('home')
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(models.Task, pk=pk)
+    task.status = 1
+    task.save()
+    return redirect('home')
+
+
+def editTask(request, pk):
+    get_task = get_object_or_404(models.Task, pk=pk)
+    if request.method == 'POST':
+        task_name = request.POST['task']
+        get_task.task = task_name
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task
+        }
+        return render(request, 'edit_task.html', context)
+
+
+def deleteTask(request, pk):
+    task = get_object_or_404(models.Task, pk=pk)
+    task.delete()
+    return redirect('home')
